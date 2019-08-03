149aea5a7d8cfa7dbd08dc2c7b768395e3101927
Nico595 <nicolas.nmillot@gmail.com>
2019-08-03 21:27:27 +0200

    laser

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    public float _laserBeamLength;
+    private LineRenderer _linerenderer;
+    
+
+    private void Start()
+    {
+        _linerenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector3 endPosition = transform.position + (transform.up * _laserBeamLength);
+        _linerenderer.SetPositions(new Vector3[] { transform.position, endPosition });
+
+        
+    }
+
+}
